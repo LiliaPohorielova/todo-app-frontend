bd4105d1bec3c511a50c04023356937bb05e91ff
SofiaRutska <rutska.sofiia@gmail.com>
2023-04-06 02:32:57 +0200

    Add Tasks

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Category} from "../model/Category";
+import {Task} from "../model/Task";
+import {TestData} from "../data/TestData";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  constructor() { }
+  taskSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+
+  categorySubject = new BehaviorSubject<Category[]>(TestData.categories);
+
+  constructor() {
+    this.fillTasks();
+  }
+
+  getCategories(): Category[] {
+    return TestData.categories;
+  }
+
+  fillTasks() {
+    this.taskSubject.next(TestData.tasks);
+  }
+
+  fillTasksByCategories(category: Category) {
+    const tasks = TestData.tasks.filter(task => task.category === category);
+    this.taskSubject.next(tasks);
+  }
 }
--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[];
+
+  constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit(): void {
+    this.dataHandler.categorySubject.subscribe(categories => this.categories = categories);
   }
 
+  showTasksByCategory(category: Category) {
+    this.dataHandler.fillTasksByCategories(category);
+  }
 }
