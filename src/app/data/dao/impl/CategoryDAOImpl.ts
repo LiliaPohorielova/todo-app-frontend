965ec8ca4a6ba145abb53e1d552da10f06b204f2
SofiaRutska <rutska.sofiia@gmail.com>
2023-04-09 12:55:21 +0200

    Add Drop-down Lists Of Categories And Priorities

--- src/app/data/dao/impl/CategoryDAOImpl.ts
+++ src/app/data/dao/impl/CategoryDAOImpl.ts
@@ -7,32 +7,26 @@ import {TestData} from "../../TestData";
 export class CategoryDAOImpl implements CategoryDAO {
 
   create(T: Category): Observable<Category> {
-    // @ts-ignore
     return undefined;
   }
 
   delete(id: number): Observable<Category> {
-    // @ts-ignore
     return undefined;
   }
 
   findAll(): Observable<Category[]> {
-    // @ts-ignore
     return of(TestData.categories);
   }
 
   findById(id: number): Observable<Category> {
-    // @ts-ignore
     return of(TestData.categories.find(cat => cat.id === id));
   }
 
   search(title: string): Observable<Category[]> {
-    // @ts-ignore
     return undefined;
   }
 
   update(T: Category): Observable<Category> {
-    // @ts-ignore
     return undefined;
   }
 }
--- src/app/data/dao/impl/PriorityDAOImpl.ts
+++ src/app/data/dao/impl/PriorityDAOImpl.ts
@@ -1,31 +1,28 @@
 import {PriorityDAO} from "../interfaces/PriorityDAO";
 import {Priority} from "../../../model/Priority";
 import { Observable } from "rxjs/internal/Observable";
+import {of} from "rxjs";
+import {TestData} from "../../TestData";
 
 export class PriorityDAOImpl implements PriorityDAO {
 
   create(T: Priority): Observable<Priority> {
-    // @ts-ignore
     return undefined;
   }
 
   delete(id: number): Observable<Priority> {
-    // @ts-ignore
     return undefined;
   }
 
   findAll(): Observable<Priority[]> {
-    // @ts-ignore
-    return undefined;
+    return of(TestData.priorities);
   }
 
   findById(id: number): Observable<Priority> {
-    // @ts-ignore
     return undefined;
   }
 
   update(T: Priority): Observable<Priority> {
-    // @ts-ignore
     return undefined;
   }
 
